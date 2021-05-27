bd675b42061a45610ca9b7b11a768de53cae308e
Solving-Enrico <cristofanillienrico@gmail.com>
2021-05-27 21:14:23 +0200

    Initial commit

---
+++ src/main/java/it/prova/datamigrator/DatamigratorApplication.java
@@ -0,0 +1,13 @@
+package it.prova.datamigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamigratorApplication.class, args);
+	}
+
+}
